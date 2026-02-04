b5521e7cdaa43e388475bf3ab34fc5adcfce190a
kasemb781-cmd <kasemb781@gmail.com>
2026-02-04 09:41:05 +0300

    App.jsx

---
+++ App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Friends Association App is Live ✅</h1>
+
+      <a href="/login">Login</a> |{" "}
+      <a href="/signup">Signup</a>
+    </div>
+  );
+}
