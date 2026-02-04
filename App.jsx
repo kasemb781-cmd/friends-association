39a6b1b7323afe86e49f426be6db8cd14ffe4e7a
kasemb781-cmd <kasemb781@gmail.com>
2026-02-04 10:56:54 +0300

    add react router pages

--- App.jsx
+++ App.jsx
@@ -1,10 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function Login() {
+  return <h2>Login Page</h2>;
+}
+
+function Signup() {
+  return <h2>Signup Page</h2>;
+}
+
+function Home() {
+  return <h1>Friends Association App is Live ✅</h1>;
+}
+
 export default function App() {
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Friends Association App is Live ✅</h1>
-
-      <a href="/login">Login</a> |{" "}
-      <a href="/signup">Signup</a>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
